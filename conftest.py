d8aaa6c4cf5932cdae35911a4069b7c9e9ade508
Iskandar Khudzhamkulov <iskandar_khudzhamkulov@epam.com>
2023-11-07 22:52:01 +0600

    changed conftest.py(1)

--- conftest.py
+++ conftest.py
@@ -5,12 +5,7 @@ from variables import *
 
 @pytest.fixture(scope='module')
 def connection_string():
-    conn_str = f"""
-                server='{SERVER}',
-                user='{USERNAME}',
-                password='{PASSWORD}',
-                database='{DATABASE}',
-                tds_version='{TDS_V}'"""
+    conn_str = f"server='{SERVER}',user='{USERNAME}',password='{PASSWORD}',database='{DATABASE}',tds_version='{TDS_V}'"
 
     yield conn_str
 
